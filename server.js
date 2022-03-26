fbebf2763bdedafd7e285a8edf91a57acd4b48a8
iminun <hehe427@naver.com>
2022-03-27 02:11:59 +0900

    detailpage생성

--- server.js
+++ server.js
@@ -39,9 +39,18 @@ app.post('/page/create_process', function(request, response){
           if(error){
             throw error;
           }
-          response.writeHead(302, {Location: `/page/${result.insertId}`});
+          response.writeHead(302, {Location: `/page/${result.insertId}`}); //result 결과에 insertId가 무엇을나타내는지
           response.end();
         }
       )
   });
+})
+
+app.get('/page/:id', function(요청, 응답){
+  var searchtitle = 요청.title;  //이 title을 
+  db.query(`SELECT * FROM 테이블명 WHERE title =${searchtitle}`, function(에러, 결과){ 
+    if(error){console.log(에러);}
+    응답.render('detailpage.ejs', {data: 결과}
+    )
+  })
 })
